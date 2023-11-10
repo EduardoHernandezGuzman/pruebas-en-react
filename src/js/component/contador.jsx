5fed4400ea6cc0e2fb2f19c44e618cafed1c0c48
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-10 16:25:29 +0000

    mejora del aspecto visual

--- src/js/component/contador.jsx
+++ src/js/component/contador.jsx
@@ -18,11 +18,23 @@ const Contador = () => {
 
     return (
         <>
-            <h1>Contador</h1>
-            <h2>{valor}</h2>
-            <button onClick={sumar} >Aumentar</button>
-            <button onClick={reestablecer} >Reestablecer</button>
-            <button onClick={restar} >Disminuir</button>
+            <div class="container text-center mt-4">
+                <div class="row align-items-start">
+                    <div class="col-6">
+                        <h1>Contador</h1>
+                        <h2>{valor}</h2>
+                        <button onClick={sumar} >Aumentar</button>
+                        <button onClick={reestablecer} >Reestablecer</button>
+                        <button onClick={restar} >Disminuir</button>
+                    </div>
+                    <div class="col-6">
+                        2
+                    </div>
+                    
+                </div>
+            </div>
+
+
         </>
     );
 };
